6edaff78251cbe4bc50bd6da3cc538249f974798
Steve-code16 <stevemolele@yahoo.com>
2024-08-15 15:15:38 +0200

    edited navigation and footer styles

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className='footer-img'>
       <img src={fragBanner} alt="footer" />
+      <p>FragInfinity &copy; {new Date().getFullYear()} All Rights Reserved</p>
     </div>
   )
 }
