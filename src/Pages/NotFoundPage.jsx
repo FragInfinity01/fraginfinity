a0826814c413a0e812b80ceabc066ac95c7469c6
Steve-code16 <stevemolele@yahoo.com>
2024-11-18 05:39:38 +0200

    added NotFoundPage and ComingSoonPage and code cleanup

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -7,7 +7,6 @@ export default function NotFoundPage() {
     <div className='not-found'>
         <h2>404</h2>
       <h1>LOST IN THE <span>MAP?</span></h1>
-      {/* <p>The page you are looking for does not exist.</p> */}
       <Link to="/" className="read-more">Return Home &nbsp;<i className="fas fa-angle-double-right"></i></Link>
       
       <VerticalText />
