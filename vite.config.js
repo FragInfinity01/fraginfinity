6aeb885448b80f18210b6e9442a8e24e296b00d4
FragInfinity01 <fragi01@outlook.com>
2025-01-15 21:22:38 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repository-name/',
-
-})
+  base: '/fraginfinity/',
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    manifest: true,
+  },
+});
